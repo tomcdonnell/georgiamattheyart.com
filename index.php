6b797dfa1ac20b77a22173cc7ba1b386ac49c66f
Tom McDonnell <tomcdonnell@gmail.com>
2013-11-02 09:17:48 +1100

    Placeholder for Georgia's Art site.

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+$url =
+(
+   'lib/tom/php/pages/contact_me/index.php?'      .
+   'backAnchorHref=http://georgiamattheyart.com&' .
+   'backAnchorText=Back&'                         .
+   'emailDstName=georgia&'                        .
+   'pageTitle=Contact Georgia Matthey&'           .
+   'pageHeading=Contact Georgia Matthey&'         .
+   'subject=Enquiry from website'
+);
+?>
+<!DOCTYPE html>
+<html>
+ <head><title>Georgia Matthey Art</title></head>
+ <body>
+  <h1>Georgia Matthey Art</h1>
+  <img src='images/georgia_sketching.jpg'/>
+  <p>Site under construction.</p>
+<?php
+/*
+  <p><a href='<?php echo $url; ?>'>Contact Georgia via email</a></p>
+*/
+?>
+  <p>Site creator: <a href='http://tomcdonnell.net'>Tom McDonnell</a>.</p>
+ </body>
+</html>
